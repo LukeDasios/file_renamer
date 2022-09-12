4eb366f64cc359582d0ccdec9e6a8a1c00fdce60
LukeDasios <luke.dasios321@gmail.com>
2022-09-11 22:01:26 -0400

    done

---
+++ README.txt
@@ -0,0 +1,21 @@
+To setup file_renamer:
+
+1.) Go to https://github.com/LukeDasios/file_renamer
+2.) Click the green button that says "code"
+3.) Click "Download Zip"
+4.) Uncompress the zip file in your desktop
+5.) Create two folders in your Desktop,
+one called "input" and one called "output".
+Spelling matters. Make sure they are spelt correctly and are lowercase.
+
+6.) Open VSCode
+7.) Open the .env file and enter your input variables
+8.) Move the files that need to be renamed and that correspond
+to your input variables into the "input" folder
+9.) In your VSCode console, type "node index.js" and press enter
+You should see output in the console with a success message if it worked
+* A potential reason it may not have worked is if you had more variables specified than files included in the input folder
+
+10.) Take the freshly named files out of the "output" folder
+11.) If ever you accidentally break the application by deleting something
+by accident, refer back to step #1 to for a fresh download
--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const fs = require("fs-extra");
 
 const PLAZA_CODE = process.env.PLAZA_CODE;
 const TENANTS = JSON.parse(process.env.TENANTS).sort();
-const TYPE = process.env.TYPE;
-const DATE = process.env.DATE;
+const TYPES = JSON.parse(process.env.TYPES);
+const DATES = JSON.parse(process.env.DATES);
 
-const dir = ".././input";
+const dir = "../.././input";
 const files = fs.readdirSync(dir);
 let i = 0;
 
 for (const file of files) {
   if (file === ".DS_Store") continue;
-  const oldDirName = `.././input/${file}`;
-  let new_name = `${PLAZA_CODE} - ${TENANTS[i]} - ${TYPE} - ${DATE}`;
-  const newDirName = `.././input/${new_name}`;
+  const oldDirName = `../.././input/${file}`;
+  let new_name = `${PLAZA_CODE} - ${TENANTS[i]} - ${TYPES[i]} - ${DATES[i]}`;
+  const newDirName = `../.././input/${new_name}`;
   i++;
 
   // rename the directory
@@ -22,14 +22,16 @@ for (const file of files) {
     if (err) {
       throw err;
     }
-
-    console.log("Naming Done!");
   });
 
   // Move the directory
-  fs.move(`.././input/${new_name}/`, `.././output/${new_name}/`, (err) => {
-    if (err) return console.error(err);
-  });
-
-  console.log("DONE!");
+  fs.move(
+    `../.././input/${new_name}/`,
+    `../.././output/${new_name}/`,
+    (err) => {
+      if (err) return console.error(err);
+    }
+  );
 }
+
+console.log(`Successfully renamed ${files.length} files!`);
